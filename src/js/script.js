4b22f78bc89899cd39a727589bfeba88d0490e7b
Max <chernuakovich96@mail.ru>
2020-03-22 00:06:49 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,53 @@
 
+// ==переменные 
 
-let money = prompt('ваш бюджет на месяц?');
-let time = prompt('введите дату в формате YYYY-MM-DD');
-let obligatoryExpenses = prompt('Введите обязательную статью расходов в этом месяце');
-let howMuch = prompt('Во сколько обойдется?');
+let  money = +prompt('ваш бюджет на месяц?'),
+     time = prompt('введите дату в формате YYYY-MM-DD');
 
+// ==объект
 
 let appData = {
-     money,
-     time,
-     expenses: {obligatoryExpenses, howMuch},
+     budjet: money,
+     timeData: time,
+     expenses: {},
      optionalExpenses: {},
      income: [],
      savings: false,
 };
+
+
+// ==перебор_элементов
+
+for(let i = 0; i < 2; i++) {
+     let a = prompt('Введите обязательную статью расходов в этом месяце', ""),
+         b = prompt('Во сколько обойдется?', "");
+
+     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null 
+         && a != '' && b != '' && a.length < 50 ) {
+          console.log('done');
+          appData.expenses[a] = b;
+     } else {
+
+     };
+     
+};
+
+appData.moneyPerDay = appData.budjet / 30;
+
+// ==вывод 
+
+alert(`ежедневный бюджет: ${appData.moneyPerDay}`);
+
+// ==условие
+
+if (appData.moneyPerDay < 100) {
+     console.log('min');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+     console.log('norm');
+} else if (appData.moneyPerDay > 2000) {
+     console.log('max');
+} else {
+     console.log('money not fount');
+};
+
 console.log(appData);
